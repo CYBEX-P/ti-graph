336e5ceef66c428181b99b25ca6b3ad236eee65b
James Schnebly <jameschnebly@gmail.com>
2019-02-22 12:13:01 -0800

    config fix

--- tiweb/config.py
+++ tiweb/config.py
@@ -6,3 +6,4 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 app.config['SECURITY_PASSWORD_HASH'] = 'sha512_crypt'
 app.config['SECURITY_PASSWORD_SALT'] = 'asfdkjasdf32cxvewfsda'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
