a5e2ec5c610c1d049f65cc1281178e1937efcd54
Jeff Springer <jeffs@cse.unr.edu>
2019-02-22 13:50:27 -0800

    added docker management utility for localhost

--- tiweb/tiweb/__init__.py
+++ tiweb/tiweb/__init__.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'notsosecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tiweb.db'
+app.config['SECURITY_PASSWORD_HASH'] = 'sha512_crypt'
+app.config['SECURITY_PASSWORD_SALT'] = 'asfdkjasdf32cxvewfsda'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create database connection object
 db = SQLAlchemy(app)
@@ -26,14 +32,21 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
             backref=db.backref('users', lazy='dynamic'))
 
+class DatabaseManagement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    port = db.Column(db.Integer)
+    ip = db.Column(db.String)
+    dockerid = db.Column(db.String)
+    status = db.Column(db.String)
+
     # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
     # Create a user to test with
 @app.before_first_request
-def create_user():
+def init_db():
     db.create_all()
-    user_datastore.create_user(email='test@foo.edu', password='password')
+#    user_datastore.create_user(email='test@foo.edu', password='password')
     db.session.commit()
 
