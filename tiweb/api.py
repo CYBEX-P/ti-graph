4941999e33cbb1fbcb40e70f8d587799f20d485c
James Schnebly <jameschnebly@gmail.com>
2019-02-25 10:58:55 -0800

    Hotfix for git nonsense

--- tiweb/api.py
+++ tiweb/api.py
@@ -3,6 +3,10 @@ from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMix
 from flask import jsonify
 from tiweb import app
 
+from gip import geoip,ASN,geoip_insert, asn_insert
+from wipe_db import wipeDB
+from runner import full_load, insertNode, insertHostname
+from whoisXML import whois, insertWhois
 from exportDB import export, processExport
 
 @app.route('/secure')
@@ -18,3 +22,46 @@ def api():
 @app.route('/neo4j/export')
 def exportNeoDB():
         return jsonify(processExport(export()))
+
+@app.route('/neo4j/load')
+def load_function():
+    full_load()
+    return "Neo4j DB loaded!"
+
+@app.route('/neo4j/wipe')
+def wipe_function():
+    wipeDB()
+    return "Neo4j DB full wipe complete!"
+
+@app.route('/neo4j/insert/<Ntype>/<data>')
+def insert(Ntype, data):
+    status = insertNode(Ntype, data)
+    if status == 1:
+        return "Success"
+    else:
+        return "Failed"
+
+@app.route('/enrich/<enrich_type>/<ip>')
+def enrich(enrich_type, ip):
+    if(enrich_type == "asn"):
+            a_results = ASN(ip)
+            status = asn_insert(a_results)
+            return str({"ASN insert status" : status})
+
+    elif enrich_type == "gip":
+            g_results = geoip(ip)
+            status = geoip_insert(g_results)
+            return str({"GIP insert status" : status})
+
+    elif enrich_type == "hostname":
+            status = insertHostname(ip)
+            return str({"Hostname insert status" : status})
+    
+    elif enrich_type == "whois":
+            w_results = whois(ip)
+            status = insertWhois(w_results)
+            return str({"Whois insert status" : status})
+            
+                
+    else:
+            return "Invalid enrichment type. Try 'asn', 'gip', or 'hostname'."
---
+++ tiweb/wipe_db.py
@@ -0,0 +1,12 @@
+# Wipe DB
+import json
+import os
+from py2neo import Graph, Node, Relationship
+
+def wipeDB():
+
+    graph = Graph("bolt://127.0.0.1:43311", auth = ('neo4j', "EiWF2bD1Mnb1u1P"))
+
+    graph.delete_all()
+    print("Deleted entire graph")
+    return 1
