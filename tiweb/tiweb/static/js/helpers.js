bfa58d2471125dba2ee7c5b6534f747465834d87
James Schnebly <jameschnebly@gmail.com>
2019-02-28 14:09:40 -0800

    Finished VERY basic frontend

--- tiweb/tiweb/static/js/helpers.js
+++ tiweb/tiweb/static/js/helpers.js
@@ -73,10 +73,55 @@ GIP = function(IP){
         if (this.readyState == 4 && this.status == 200) {
         
             var data = JSON.parse(xhttp.responseText);
-            exportDB();
+            if(data["GIP insert status"] != 0){
+                exportDB();
+            }
+            else{
+                alert("Geoip lookup returned nothing.");
+            }
         }
     }
     xhttp.open("GET", `http://localhost:5000/enrich/gip/${IP}`, true);
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", '*');
+    xhttp.send();
+}
+
+whois = function(IP){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        
+            var data = JSON.parse(xhttp.responseText);
+            if(data["Whois insert status"] != 0){
+                exportDB();
+            }
+            else{
+                alert("Whois lookup returned nothing.");
+            }
+
+        }
+    }
+    xhttp.open("GET", `http://localhost:5000/enrich/whois/${IP}`, true);
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", '*');
+    xhttp.send();
+}
+
+host = function(IP){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        
+            var data = JSON.parse(xhttp.responseText);
+            if(data["Hostname insert status"] != 0){
+                exportDB();
+            }
+            else{
+                alert("Hostname lookup returned nothing.");
+            }
+            
+        }
+    }
+    xhttp.open("GET", `http://localhost:5000/enrich/hostname/${IP}`, true);
     xhttp.setRequestHeader("Access-Control-Allow-Origin", '*');
     xhttp.send();
 }
