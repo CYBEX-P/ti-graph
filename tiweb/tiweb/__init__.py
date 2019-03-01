45c1031c264b8e455ac45b5e2de24366eb6392f7
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-28 17:15:27 -0800

    Got default static route working

--- tiweb/tiweb/__init__.py
+++ tiweb/tiweb/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
-app = Flask(__name__, static_folder='react_build/static', template_folder='react_build')
+app = Flask(__name__, static_folder='static', template_folder='static')
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'notsosecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tiweb.db'
