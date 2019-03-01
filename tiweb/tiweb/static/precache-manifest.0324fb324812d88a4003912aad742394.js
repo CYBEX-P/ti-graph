21bfa850845a65b7a8c3f9d33b36d657bcdee719
Nathan Yocum <nayocum@nevada.unr.edu>
2019-03-01 11:58:34 -0800

    Updated frontend build

---
+++ tiweb/tiweb/static/precache-manifest.0324fb324812d88a4003912aad742394.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0ea881c7efc74020b246",
+    "url": "/static/js/main.0ea881c7.chunk.js"
+  },
+  {
+    "revision": "e5cdf44fad1f56ff2a0d",
+    "url": "/static/js/2.e5cdf44f.chunk.js"
+  },
+  {
+    "revision": "77eb6e5b2445357c166544273bbe1c19",
+    "url": "/index.html"
+  }
+];
--- tiweb/tiweb/static/service-worker.js
+++ tiweb/tiweb/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.acd0e5d3dadac31c39d63916ac43b80f.js"
+  "/precache-manifest.0324fb324812d88a4003912aad742394.js"
 );
 
 workbox.clientsClaim();
