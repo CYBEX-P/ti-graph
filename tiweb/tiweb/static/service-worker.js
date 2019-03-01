f5a0e1e50bc45519f3a38215694037ad76154906
Nathan Yocum <nayocum@nevada.unr.edu>
2019-03-01 14:29:57 -0800

    Updated frontend build. Added ability to enrich

--- tiweb/tiweb/static/service-worker.js
+++ tiweb/tiweb/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0324fb324812d88a4003912aad742394.js"
+  "/precache-manifest.36c941444dc8691add9793f2d0eebe44.js"
 );
 
 workbox.clientsClaim();
