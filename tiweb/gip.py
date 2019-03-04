4308b1334f1f79b660f4778841c46cadf407a540
Nathan Yocum <nayocum@nevada.unr.edu>
2019-03-04 11:36:11 -0800

    Fixed merge errors

--- tiweb/gip.py
+++ tiweb/gip.py
@@ -1,27 +1,25 @@
 import geoip2.database
-from py2neo import Node, Relationship  # , Graph
+from py2neo import Graph, Node, Relationship
 import json
 import os
 
 from parser import pull_ip_src
 
-
 def geoip(ip):
-    reader = geoip2.database.Reader('./tiweb/data/GeoLite2-City.mmdb')
+    reader = geoip2.database.Reader('/home/jschnebly/Desktop/ti-graph/tiweb/data/GeoLite2-City.mmdb')
 
     insights = {}
-    try:
+    try: 
         response = reader.city(ip)
         reader.close()
 
         insights["ip_src"] = ip
         insights["country"] = response.country.name
-        insights[
-            "state_subdivision"] = response.subdivisions.most_specific.name
+        insights["state_subdivision"] = response.subdivisions.most_specific.name
         insights["city"] = response.city.name
 
         return insights
-    except Exception:
+    except:
         reader.close()
         return 0
 
@@ -32,24 +30,26 @@ def geoip_insert(data, graph):
                 ip_node = graph.nodes.match("IP", IP=data["ip_src"]).first()
                 c_node = graph.nodes.match("Country", country = data["country"]).first()
 
-        if (c_node):
-            rel = Relationship(ip_node, "IS_LOCATED_IN", c_node)
-            graph.create(rel)
-            print("Existing country node linked")
+                if(c_node):
+                        rel = Relationship(ip_node, "IS_LOCATED_IN", c_node)
+                        graph.create(rel)
+                        print("Existing country node linked")
+                else:
+                        graph.create(c)
+                        rel = Relationship(ip_node, "IS_LOCATED_IN", c)
+                        graph.create(rel)
+                        print("New country node created and linked")
+                return 1
         else:
-            graph.create(c)
-            rel = Relationship(ip_node, "IS_LOCATED_IN", c)
-            graph.create(rel)
-            print("New country node created and linked")
-        return 1
-    else:
-        print("No GeoIP Entry for {}".format(data["ip_src"]))
-        return 0
+                print("No GeoIP Entry for {}".format(data["ip_src"]))
+                return 0
+        
 
+    
 
 def ASN(ip):
-    with geoip2.database.Reader('./tiweb/data/GeoLite2-ASN.mmdb') as reader:
-
+    with geoip2.database.Reader('/home/jschnebly/Desktop/ti-graph/tiweb/data/GeoLite2-ASN.mmdb') as reader:
+        
         insights = {}
         try:
             response = reader.asn(ip)
@@ -57,7 +57,7 @@ def ASN(ip):
             insights["ASO"] = response.autonomous_system_organization
             insights["ip_src"] = ip
             return insights
-        except Exception:
+        except:
             return 0
 
 def asn_insert(data, graph):
@@ -67,16 +67,16 @@ def asn_insert(data, graph):
                 ip_node = graph.nodes.match("IP", IP=data["ip_src"]).first()
                 a_node = graph.nodes.match("ASN", asn = data["ASN"]).first()
 
-        if (a_node):
-            rel = Relationship(ip_node, "HAS_ASN", a_node)
-            graph.create(rel)
-            print("Existing asn node linked")
+                if(a_node):
+                        rel = Relationship(ip_node, "HAS_ASN", a_node)
+                        graph.create(rel)
+                        print("Existing asn node linked")
+                else:
+                        graph.create(a)
+                        rel = Relationship(ip_node, "HAS_ASN", a)
+                        graph.create(rel)
+                        print("New asn node created and linked")
+                return 1
         else:
-            graph.create(a)
-            rel = Relationship(ip_node, "HAS_ASN", a)
-            graph.create(rel)
-            print("New asn node created and linked")
-        return 1
-    else:
-        print("No asn Entry for {}".format(data["ip_src"]))
-        return 0
+                print("No asn Entry for {}".format(data["ip_src"]))
+                return 0
