fbadfb0b98331c23547c5bc6bc2793410e012290
James Schnebly <jameschnebly@gmail.com>
2019-03-04 09:41:31 -0800

    quick fix

--- tiweb/connect.py
+++ tiweb/connect.py
@@ -1,3 +1,3 @@
 from py2neo import Graph
-graph = Graph("bolt://127.0.0.1:54238", auth = ('neo4j', "bKj6zhPMKuprCqi"))
+graph = Graph("bolt://127.0.0.1:56240", auth = ('neo4j', "bn2328ogaGEbXNo"))
 #graph = Graph("bolt://cybexp2.acs.unr.edu:7687", auth = ('neo4j', "cybexp19"))
