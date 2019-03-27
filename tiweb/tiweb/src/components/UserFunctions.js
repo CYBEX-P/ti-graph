571173d758d8b23123c5b87f3ef27cc55a0bb7d3
ankitath <47464189+ankitath@users.noreply.github.com>
2019-03-27 13:56:12 -0700

    React Front End + User Management

---
+++ tiweb/tiweb/src/components/UserFunctions.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+//import cookie from 'react-cookie'
+
+
+
+
+export const register = newUser => {
+    return axios
+    .post('/users/register', {
+        first_name: newUser.first_name,
+        last_name: newUser.last_name,
+        email: newUser.email,
+        username: newUser.username,
+        password: newUser.password
+    })
+    .then(response =>{
+        console.log('registered')
+    })
+}
+
+export const login = user => {
+    return axios.post('users/login',{
+        username: user.username,
+        password: user.pasword
+
+    })
+    .then(res=> {
+        localStorage.setItem('token', res.data.token)
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const remove = user => {
+    return axios.post('/delete',{
+        username: user.username,
+        
+
+    })
+    .then(res=> {
+        localStorage.setItem('token', res.data.token)
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const update = user => {
+    return axios.post('/update',{
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        username: user.username,
+        
+
+    })
+    .then(res=> {
+        localStorage.setItem('token', res.data.token)
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const find = user => {
+    return axios.post('/find_user',{
+        
+        email: user.email  
+
+    })
+    .then(res=> {
+        localStorage.setItem('token', res.data.token)
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+
+
